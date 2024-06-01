fca723f0ec8df73f366abf871970906d01fa45ca
Joselu <joseluissgz97@gmail.com>
2024-06-01 23:18:40 +0200

    feat: Add App component with main layout and UserForm integration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import UserForm from "./components/UserForm";
+
+const App = () => {
+	return (
+		<main className="wrapper">
+			<h1 className="title">React hook form and Zod</h1>
+			<UserForm />
+		</main>
+	);
+};
+
+export default App;
