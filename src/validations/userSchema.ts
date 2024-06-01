a11764c0d6f7881f6dd0c6f3e58287ccd166c86d
Joselu <joseluissgz97@gmail.com>
2024-06-01 23:18:10 +0200

    feat: Add userSchema validation for user input

---
+++ src/validations/userSchema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+const PLANS = ["basic", "premium", "gold"] as const;
+
+export type Plans = (typeof PLANS)[number];
+export const mappedPlans: { [key in Plans]: string } = {
+	basic: "Basic",
+	premium: "Premium",
+	gold: "Gold",
+};
+
+export const userShema = z.object({
+	name: z.string().min(1, { message: "Camp required" }),
+	email: z
+		.string()
+		.email({ message: "Invalid email" })
+		.min(1, { message: "Camp required" }),
+	password: z.string().min(1, { message: "Camp required" }),
+	confirmPassword: z.string().min(1, { message: "Camp required" }),
+	weight: z
+		.string()
+		.min(1, { message: "Camp required" })
+		.refine((weight) => !isNaN(parseFloat(weight))),
+	plan: z.enum(PLANS),
+});
+
+export type User = z.infer<typeof userShema>;
