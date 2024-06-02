29c4a825b7e8f867c61b577534171e902e30332a
Joselu <joseluissgz97@gmail.com>
2024-06-02 20:52:53 +0200

    chore: Update App component to include navigation and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import "./App.css";
-import UserForm from "./components/UserForm";
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import UserForm from './components/UserForm';
+import DataFrom from './components/dataForm/DataFrom';
+import NavList from './components/nav/NavList';
 
 const App = () => {
-	return (
-		<main className="wrapper">
-			<h1 className="title">React hook form and Zod</h1>
-			<UserForm />
-		</main>
-	);
+  return (
+    <main className="wrapper">
+      <h1 className="title">React hook form and Zod</h1>
+
+      <NavList />
+      <Routes>
+        <Route path="/user-form" index element={<UserForm />} />
+        <Route path="/data-form" index element={<DataFrom />} />
+
+        <Route path="*" element={<Navigate to={'/user-form'} replace />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
