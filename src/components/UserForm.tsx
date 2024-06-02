9fa494b12ff4a0ef066d31f801a101c550e29668
Joselu <joseluissgz97@gmail.com>
2024-06-02 20:53:41 +0200

    feat: Refactor UserForm component to use common Input component

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -2,6 +2,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { User, mappedPlans, userShema } from '../validations/userSchema';
 import style from './UserForm.module.css';
+import ButtonSubmit from './buttonSubmit/ButtonSubmit';
+import Input from './common/Input';
 import WarningIcon from './icons/WarningIcon';
 
 const UserForm = () => {
@@ -19,98 +21,45 @@ const UserForm = () => {
   ));
   console.log(errors);
   return (
-    <form className={style.form} onSubmit={handleSubmit((data) => console.log(data))}>
-      <div className={style.row}>
-        <label className={`${style.row__label} ${errors.name ? style.row__error : ''}`} htmlFor="name">
-          name*
-        </label>
-        <div className={`${style.row__container} ${errors.name ? style.row__error : ''}`}>
-          <input
-            className={style.row__input}
-            id="name"
-            type="text"
-            autoComplete="off"
-            placeholder="example: Pedro"
-            {...register('name')}
-          />
-          {errors.name && <WarningIcon className={style.row__icon} />}
-        </div>
-        <span className={style.row__message}>{errors.name?.message} </span>
-      </div>
-      <div className={style.row}>
-        <label className={`${style.row__label} ${errors.email ? style.row__error : ''}`} htmlFor="email">
-          Email
-        </label>
-        <div className={`${style.row__container} ${errors.email ? style.row__error : ''}`}>
-          <input
-            className={style.row__input}
-            id="email"
-            type="email"
-            autoComplete="off"
-            placeholder="example: Pedro@email.com"
-            {...register('email')}
-          />
-          {errors.email && <WarningIcon className={style.row__icon} />}
-        </div>
-        <span className={style.row__message}>{errors.email?.message}</span>
-      </div>
-      <div className={style.row}>
-        <label
-          className={`${style.row__label} ${errors.password ? style.row__error : ''}`}
-          htmlFor="password"
-        >
-          Password*
-        </label>
-        <div className={`${style.row__container} ${errors.password ? style.row__error : ''}`}>
-          <input
-            className={style.row__input}
-            id="password"
-            type="password"
-            autoComplete="off"
-            placeholder="your password"
-            {...register('password')}
-          />
-          {errors.password && <WarningIcon className={style.row__icon} />}
-        </div>
-        <span className={style.row__message}>{errors.password?.message}</span>
-      </div>
-      <div className={style.row}>
-        <label
-          className={`${style.row__label} ${errors.confirmPassword ? style.row__error : ''}`}
-          htmlFor="name"
-        >
-          Confirm Password
-        </label>
-        <div className={`${style.row__container} ${errors.confirmPassword ? style.row__error : ''}`}>
-          <input
-            className={style.row__input}
-            id="confirmPassword"
-            type="password"
-            autoComplete="off"
-            placeholder="your password"
-            {...register('confirmPassword')}
-          />
-          {errors.confirmPassword && <WarningIcon className={style.row__icon} />}
-        </div>
-        <span className={style.row__message}>{errors.confirmPassword?.message}</span>
-      </div>
-      <div className={style.row}>
-        <label className={`${style.row__label} ${errors.weight ? style.row__error : ''}`} htmlFor="name">
-          Weight
-        </label>
-        <div className={`${style.row__container} ${errors.weight ? style.row__error : ''}`}>
-          <input
-            className={style.row__input}
-            id="weight"
-            type="text"
-            autoComplete="off"
-            placeholder="22"
-            {...register('weight')}
-          />
-          {errors.weight && <WarningIcon className={style.row__icon} />}
-        </div>
-        <span className={style.row__message}>{errors.weight?.message}</span>
-      </div>
+    <form className="form" onSubmit={handleSubmit((data) => console.log(data))}>
+      <Input
+        id="name"
+        label="Name"
+        type="tex"
+        placeholder="explame: Pedro"
+        error={errors['name']}
+        {...register('name')}
+      />
+      <Input
+        id="email"
+        label="Email"
+        placeholder="example:pedro@pedor.com"
+        error={errors['email']}
+        {...register('email')}
+      />
+
+      <Input
+        id="password"
+        label="Password"
+        placeholder="Your password"
+        error={errors['password']}
+        {...register('password')}
+      />
+      <Input
+        id="confirmPassword"
+        label="confirmPassword"
+        placeholder="Your password"
+        error={errors['confirmPassword']}
+        {...register('confirmPassword')}
+      />
+
+      <Input
+        id="weight"
+        label="Weight"
+        placeholder="Weight example:55"
+        error={errors['weight']}
+        {...register('weight')}
+      />
       <div className={style.row}>
         <label className={`${style.row__label} ${errors.plan ? style.row__error : ''}`} htmlFor="name">
           Plan
@@ -124,9 +73,7 @@ const UserForm = () => {
         <span className={style.row__message}>{errors.plan?.message}</span>
       </div>
 
-      <button type="submit" className={style.form__submit}>
-        Send
-      </button>
+      <ButtonSubmit label={'send'} />
     </form>
   );
 };
